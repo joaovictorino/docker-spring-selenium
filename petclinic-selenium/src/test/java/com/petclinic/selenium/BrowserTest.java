fa5ba252df34c5bc6062342710561c26988bd905
joaovictorino <jhvictorino@gmail.com>
2021-03-30 16:16:30 -0300

    selenium first test

--- petclinic-selenium/src/test/java/com/petclinic/selenium/BrowserTest.java
+++ petclinic-selenium/src/test/java/com/petclinic/selenium/BrowserTest.java
@@ -13,8 +13,6 @@ public class BrowserTest {
         options.addArguments("--start-maximized"); // open Browser in maximized mode
         options.addArguments("--disable-infobars"); // disabling infobars
         options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("--no-sandbox"); // Bypass OS security model
         WebDriver browser = new ChromeDriver(options);
         browser.navigate().to("http://localhost:8080");
     }
