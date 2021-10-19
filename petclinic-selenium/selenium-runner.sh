0d5b977e11ae4d6963ddedcb3ab12288fb696d8d
joaovictorino <jhvictorino@gmail.com>
2021-10-19 00:42:44 -0300

    selenium grid

---
+++ petclinic-selenium/selenium-runner.sh
@@ -0,0 +1,8 @@
+# selenium grid up
+docker-compose up
+
+# build selenium runner
+docker build -t selenium-runner .
+
+# selenium-side-runner
+docker run -it --rm -v /home/joao/Source/Test/selenium-spring-tests/petclinic-selenium/script:/script --network=petclinic-selenium_selenium selenium-runner selenium-side-runner --server http://selenium-hub:4444/wd/hub /script/petclinic.side --base-url http://172.17.0.1:8080
